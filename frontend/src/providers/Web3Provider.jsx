c34e3ad2b549a5ba57f8a096c24eee7b21b9a320
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-10 20:01:26 +0530

    Add AGW provider

--- frontend/src/providers/Web3Provider.jsx
+++ frontend/src/providers/Web3Provider.jsx
@@ -1,1 +1,10 @@
+import { AbstractWalletProvider } from "@abstract-foundation/agw-react";
+import { abstractTestnet } from "viem/chains";
 
+export function Web3Provider({ children }) {
+  return (
+    <AbstractWalletProvider chain={abstractTestnet}>
+      {children}
+    </AbstractWalletProvider>
+  );
+}
