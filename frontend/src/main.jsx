1f0ddd09667d138fbd90a96b9b2af9dafb9b4a87
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-10 20:00:53 +0530

    Add React main entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Web3Provider } from "./providers/Web3Provider.jsx";
+import "./styles/global.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Web3Provider>
+      <App />
+    </Web3Provider>
+  </React.StrictMode>
+);
