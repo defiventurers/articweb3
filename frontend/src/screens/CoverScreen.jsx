8a93d1032bb547347e47ccb52b292708c2df5e79
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-10 20:02:26 +0530

    Add cover screen

--- frontend/src/screens/CoverScreen.jsx
+++ frontend/src/screens/CoverScreen.jsx
@@ -1,1 +1,12 @@
-
+export function CoverScreen({ onContinue }) {
+  return (
+    <section className="screen">
+      <button className="cover-screen" onClick={onContinue}>
+        <div>
+          <h1>Artic Web3</h1>
+          <p>Tap to continue</p>
+        </div>
+      </button>
+    </section>
+  );
+}
