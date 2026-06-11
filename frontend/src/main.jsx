ed87e47e6001b2347b8fa3ae115e41c3fd578686
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-11 20:29:13 +0530

    Import profile screen styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import "./styles/lobbyModes.css";
 import "./styles/highStakes.css";
 import "./styles/gameBoard.css";
 import "./styles/teamSelect.css";
+import "./styles/profileScreen.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
