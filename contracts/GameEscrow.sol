f2d2c10585ac0201ffec6b20860ac844e6b1bc6d
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-11 05:48:47 +0530

    Add entry locking scaffold

--- contracts/GameEscrow.sol
+++ contracts/GameEscrow.sol
@@ -11,14 +11,19 @@ contract GameEscrow {
     IERC20 public immutable token;
 
     mapping(address => uint256) public availableBalance;
+    mapping(address => uint256) public lockedBalance;
+    mapping(bytes32 => mapping(address => uint256)) public lockedInMatch;
 
     bool private locked;
 
     event Deposited(address indexed player, uint256 amount);
     event Withdrawn(address indexed player, uint256 amount);
+    event EntryLocked(bytes32 indexed matchId, address indexed player, uint256 amount);
+    event EntryReleased(bytes32 indexed matchId, address indexed player, uint256 amount);
 
     error InvalidToken();
     error InvalidAmount();
+    error InvalidMatch();
     error TransferFailed();
     error InsufficientBalance();
     error ReentrantCall();
@@ -56,6 +61,31 @@ contract GameEscrow {
         if (!ok) revert TransferFailed();
 
         emit Withdrawn(msg.sender, amount);
+    }
+
+    function lockEntry(bytes32 matchId, uint256 amount) external nonReentrant {
+        if (matchId == bytes32(0)) revert InvalidMatch();
+        if (amount == 0) revert InvalidAmount();
+        if (availableBalance[msg.sender] < amount) revert InsufficientBalance();
+
+        availableBalance[msg.sender] -= amount;
+        lockedBalance[msg.sender] += amount;
+        lockedInMatch[matchId][msg.sender] += amount;
+
+        emit EntryLocked(matchId, msg.sender, amount);
+    }
+
+    function releaseEntry(bytes32 matchId) external nonReentrant {
+        if (matchId == bytes32(0)) revert InvalidMatch();
+
+        uint256 amount = lockedInMatch[matchId][msg.sender];
+        if (amount == 0) revert InvalidAmount();
+
+        lockedInMatch[matchId][msg.sender] = 0;
+        lockedBalance[msg.sender] -= amount;
+        availableBalance[msg.sender] += amount;
+
+        emit EntryReleased(matchId, msg.sender, amount);
     }
 
     function escrowTokenBalance() external view returns (uint256) {
