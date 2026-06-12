1c7766c25d3fc2a4ce44be8451f1add61e3fbeac
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-12 19:51:09 +0530

    Configure Hardhat for Abstract testnet

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@matterlabs/hardhat-zksync";
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "latest"
+  },
+  defaultNetwork: "abstractTestnet",
+  networks: {
+    abstractTestnet: {
+      url: "https://api.testnet.abs.xyz",
+      ethNetwork: "sepolia",
+      zksync: true,
+      chainId: 11124
+    },
+    abstractMainnet: {
+      url: "https://api.mainnet.abs.xyz",
+      ethNetwork: "mainnet",
+      zksync: true,
+      chainId: 2741
+    }
+  },
+  solidity: {
+    version: "0.8.24"
+  }
+};
+
+export default config;
