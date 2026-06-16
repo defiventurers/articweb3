e912a82cc25995c21ddf79efe395a2e41b1ee99d
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-06-17 01:22:01 +0530

    Load Frost Gate loading styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Web3Provider } from "./providers/Web3Provider.jsx";
 import "./styles/global.css";
+import "./styles/frostLoading.css";
 import "./styles/lobbyModes.css";
 import "./styles/highStakes.css";
 import "./styles/gameBoard.css";
