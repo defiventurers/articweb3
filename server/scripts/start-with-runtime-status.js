caa381106d37f9a32a221233c05a288fcf4c7fc7
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-07-25 02:36:30 +0530

    Install Sixteen Ice Warriors backend before server boot

--- server/scripts/start-with-runtime-status.js
+++ server/scripts/start-with-runtime-status.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const { installRuntimeStatusEndpoints } = require("../runtimeStatusBootstrap.js");
+const { installSixteenIceWarriorsBackend } = require("../sixteenIceWarriorsBackendBootstrap.js");
 
 installRuntimeStatusEndpoints();
+installSixteenIceWarriorsBackend();
 require("./start-with-indexer.js");
