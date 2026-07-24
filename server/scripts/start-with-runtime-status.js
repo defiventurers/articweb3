44dfd6f562ff9a89d1ac38c54b5c7a37c32cd323
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-07-25 02:43:42 +0530

    Keep runtime launcher unchanged

--- server/scripts/start-with-runtime-status.js
+++ server/scripts/start-with-runtime-status.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
 const { installRuntimeStatusEndpoints } = require("../runtimeStatusBootstrap.js");
-const { installSixteenIceWarriorsBackend } = require("../sixteenIceWarriorsBackendBootstrap.js");
 
 installRuntimeStatusEndpoints();
-installSixteenIceWarriorsBackend();
 require("./start-with-indexer.js");
