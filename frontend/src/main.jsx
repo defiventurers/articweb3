7635b8319cfc9c37f74ec33dc0ac5016fc700967
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-07-25 07:25:01 +0530

    Load Sky Temple Run styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import "./styles/glacierTrail.css";
 import "./styles/crownRun.css";
 import "./styles/crownRunInteraction.css";
 import "./styles/fortyGlacierGuards.css";
+import "./styles/skyTempleRun.css";
 import "./styles/frostLoading.css";
 import "./styles/lobbyModes.css";
 import "./styles/highStakes.css";
