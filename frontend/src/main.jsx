98d29be25d9132eddf65e153f860639bfebb964b
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-07-29 08:42:40 +0530

    Load Khasi Fishflow styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,6 +29,7 @@ import "./styles/polarTablan.css";
 import "./styles/auroraGanjifaAcademy.css";
 import "./styles/sige.css";
 import "./styles/sevenIceRings.css";
+import "./styles/khasiFishflow.css";
 import "./styles/frostLoading.css";
 import "./styles/lobbyModes.css";
 import "./styles/highStakes.css";
